cce7f7c349421f13a419f04215b120dc070a2389
Mario Antolovic <marioantolovic0@gmail.com>
2019-05-24 21:56:27 +0200

    league activity skills activity
    fixed small warnings

--- app/src/main/java/mario/antolovic/baller/MainActivity.kt
+++ app/src/main/java/mario/antolovic/baller/MainActivity.kt
@@ -1,12 +1,19 @@
 package mario.antolovic.baller
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getStartedButton.setOnClickListener {
+            val leagueIntent = Intent(this,LeagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
     }
 }
